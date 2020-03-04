3036372d1a3f47d64ff66157babaf112722f167a
Joshua Boyd <jaboyd0@gmail.com>
2020-03-04 00:57:48 -0500

    got quiz to progress through questions

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ var question = document.querySelector("#question")
 var answers = document.querySelector("#answers")
 var currentScore = document.querySelector("#currentScore")
 var button = document.querySelector("#button")
+var navButtons = document.querySelector("#navButtons")
 
+
+var questionNum = 0
 var score = 0
+var questionComplete = false
 var scoreHistory
 var questionAnswer 
+var selectedAnswer
+
+
 
 var quizQuestions = [{
 
@@ -22,61 +29,99 @@ var quizQuestions = [{
     answer: "1"
 },
 {
-    question: "which one is 2?",
+    question: "which one is 6?",
     respOptions: [
-        "1",
-        "2",
-        "3",
-        "4"
+        "5",
+        "6",
+        "7",
+        "8"
     ],
-    answer: "2"
+    answer: "6"
+},
+{
+    question: "which one is 11?",
+    respOptions: [
+        "9",
+        "10",
+        "11",
+        "12"
+    ],
+    answer: "11"
+},
+{
+    question: "which one is 16?",
+    respOptions: [
+        "13",
+        "14",
+        "15",
+        "16"
+    ],
+    answer: "16"
 }
 ];
 
 function addButton1() {
     var button1 = document.createElement("button")
     button1.id = "button1"
-    button1.class = "btn btn-primary"
+    button1.className = "answerButtons btn "
     button1.innerText = "Button A"
+    button1.value = quizQuestions[questionNum].respOptions[0]
     answers.appendChild(button1)
 }
 function addButton2() {
     var button2 = document.createElement("button")
     button2.id = "button2"
-    button2.class = "btn btn-primary"
+    button2.className = "answerButtons btn "
     button2.innerText = "Button B"
+    button2.value = quizQuestions[questionNum].respOptions[1]
     answers.appendChild(button2)
 }
 function addButton3() {
     var button3 = document.createElement("button")
     button3.id = "button3"
-    button3.class = "btn btn-primary"
+    button3.className = "answerButtons btn "
     button3.innerText = "Button C"
+    button3.value = quizQuestions[questionNum].respOptions[2]
     answers.appendChild(button3)
 }
 function addButton4() {
     var button4 = document.createElement("button")
     button4.id = "button4"
-    button4.class = "btn btn-primary"
+    button4.className = "answerButtons btn "
     button4.innerText = "Button D"
+    button4.value = quizQuestions[questionNum].respOptions[3]
     answers.appendChild(button4)
+}
+function addNextButton() {
+    var nextButton = document.createElement("button")
+    nextButton.id = "nextButton"
+    nextButton.className = "btn btn-primary"
+    nextButton.innerText = "Next"
+    navButtons.appendChild(nextButton)
 }
 
 function inportQuestions () {
-    console.log(quizQuestions[i].question)
-    question.textContent=quizQuestions[i].question
-    document.getElementById("button1").innerText=quizQuestions[i].respOptions[0]
-    document.getElementById("button2").innerText=quizQuestions[i].respOptions[1]
-    document.getElementById("button3").innerText=quizQuestions[i].respOptions[2]
-    document.getElementById("button4").innerText=quizQuestions[i].respOptions[3]
+    question.textContent=quizQuestions[questionNum].question
+    document.getElementById("button1").innerText=quizQuestions[questionNum].respOptions[0]
+    document.getElementById("button1").value=quizQuestions[questionNum].respOptions[0]
+
+    document.getElementById("button2").innerText=quizQuestions[questionNum].respOptions[1]
+    document.getElementById("button2").value=quizQuestions[questionNum].respOptions[1]
+    
+    document.getElementById("button3").innerText=quizQuestions[questionNum].respOptions[2]
+    document.getElementById("button3").value=quizQuestions[questionNum].respOptions[2]
+
+    document.getElementById("button4").innerText=quizQuestions[questionNum].respOptions[3]
+    document.getElementById("button4").value=quizQuestions[questionNum].respOptions[3]
+    return
 }
 
-var secondsLeft = 50;
+var secondsLeft = 30;
 function setTime() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timer.textContent = ` ${secondsLeft} seconds`;
-        if (secondsLeft <= 0 || quizOver) {
+        if (secondsLeft <= 0 ) {
             // if (scoreHistory.length !== 0) {
             //     document.getElementById("view-scores").removeAttribute("disabled");
             // }
@@ -87,27 +132,117 @@ function setTime() {
     }, 1000);
 }
 
+function correctAnswer() {
+    if (questionComplete == false){
+    questionComplete = true
+    score ++
+    document.getElementById("currentScore").innerText = score
+    document.getElementById("result").innerText = "Correct Answer!"
+}
+else{return}
+
+}
+
+function wrongAnswer() {
+    if (questionComplete == false){
+    questionComplete = true
+    score 
+    document.getElementById("currentScore").innerText = score
+    document.getElementById("result").innerText = "You got it wrong."
+}
+else{return}
+}
+
+function nextQuestion() {
+    questionNum ++
+    questionComplete= false
+    if (questionNum!==quizQuestions.length){
+    inportQuestions()
+    document.getElementById("result").innerText = ""
+    return
+}
+else{endOfQuiz()}
+}
+
+function buttonMechanics() {
+    
+    $("#button1").click(function() {
+        console.log(this.getAttribute("value"));
+        if (this.getAttribute("value")==quizQuestions[questionNum].answer) {
+            console.log("correct answer")
+            correctAnswer()
+            return
+       } else {
+           console.log("Wrong Answer")
+           wrongAnswer()
+       }
+      })
+    
+      $("#button2").click(function() {
+        console.log(this.getAttribute("value"));
+        if (this.getAttribute("value")==quizQuestions[questionNum].answer) {
+            console.log("Correct Answer")
+            correctAnswer()
+            return
+       } else {
+           console.log("Wrong Answer")
+           wrongAnswer()
+       }
+      })
+
+      $("#button3").click(function() {
+        console.log(this.getAttribute("value"));
+        if (this.getAttribute("value")==quizQuestions[questionNum].answer) {
+            console.log("correct answer")
+            correctAnswer()
+            return
+       } else {
+           console.log("Wrong Answer")
+           wrongAnswer()
+       }
+      })
+  
+      $("#button4").click(function() {
+        console.log(this.getAttribute("value"));
+        if (this.getAttribute("value")==quizQuestions[questionNum].answer) {
+            console.log("correct answer")
+            correctAnswer()
+            return
+       } else {
+           console.log("Wrong Answer")
+           wrongAnswer()
+       }
+      })
+}
+
+function endOfQuiz(){
+    $('#modal').modal()
+    
+}
 
 function quiz() {
 
     answers.innerHTML = "";
-
     addButton1()
     addButton2()
     addButton3()
     addButton4()
+    addNextButton()
 
     setTime()
 
-    for (i = 0; i < quizQuestions.length; i++) {
+    // for (i = 0; i < quizQuestions.length; i++) {
      
-        inportQuestions()
+    inportQuestions()
         
-
-      }
+    buttonMechanics()
     
+    
+    $("#nextButton").click(nextQuestion)
+    return
 
 }
+
 
 // document.getElementById("button").addEventListener("click", Quiz() 
     
@@ -117,36 +252,3 @@ button.addEventListener("click", quiz);
 
 
 
-
-
-
-
-// not being used
-    // var questions = [
-    //   { q: "The sky is blue.", a: "t" },
-    //   { q: "There are 365 days in a year.", a: "t" },
-    //   { q: "There are 42 ounces in a pound.", a: "f" },
-    //   { q: "The Declaration of Independence was created in 1745.", a: "f" },
-    //   { q: "Bananas are vegetables.", a: "f" }
-    // ];
-
-    // var score = 0;
-
- 
-    // for (var i = 0; i < questions.length; i++) {
-    //   // Display current question to user and ask OK/Cancel
-    //   var answer = confirm(questions[i].q);
-
-    //   // Compare answers
-    //   if ((answer === true && questions[i].a === "t") ||
-    //     (answer === false && questions[i].a === "f")) {
-    //     // Increase score
-    //     score++;
-    //     alert("Correct!");
-    //   }
-    //   else {
-    //     alert("Wrong!");
-    //   }
-    // }
-
-    // alert("You got " + score + "/" + questions.length);
